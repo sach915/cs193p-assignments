159df1ee67afd82f6da7f70407ea2480107338b0
sach915 <sach915@gmail.com>
2017-07-22 13:20:07 -0400

    First commit

--- Calculator2/CalculatorBrain.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CalculatorBrain.swift
-//  Calculator2
-//
-//  Created by Sach Vaidya on 7/22/17.
-//  Copyright © 2017 Sach Vaidya. All rights reserved.
-//
-
-import Foundation
--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
-
-var a : Dictionary<String,Int> = ["hello":2]
-
-print(a["hello"])
